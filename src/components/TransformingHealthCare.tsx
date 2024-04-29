cd1081e6939cc6d5afe8524869deac84e6f11865
Blizz <ellamsbliss@gmail.com>
2024-04-29 14:01:51 +0100

    transforming section text

---
+++ src/components/TransformingHealthCare.tsx
@@ -0,0 +1,25 @@
+const TransformingHealthCare = () => {
+  return (
+    <section className="px-8 md:px-12 lg:px-32 py-4">
+      <div className="flex flex-col gap-4">
+        {/* head */}
+        <h3 className="text-center lg:text-4xl md:text-2xl text-2xl font-extrabold ">
+          {" "}
+          Transforming Healthcare Delivery
+        </h3>
+        {/* content */}
+        <p className="text-xl md:text-2xl font-light">
+          Welcome to the future of healthcare. Our state-of-the-art Electronic
+          Health Records (EHR) system is designed to revolutionize how
+          healthcare is delivered, managed, and experienced across hospitals and
+          and patient care facilities. By integrating advanced digital
+          technology with the unique needs of healthcare facilities, we offer a
+          comprehensive solution that enhances patient care, improves
+          operational efficiency, and ensures secure, accessible medical data.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default TransformingHealthCare;
